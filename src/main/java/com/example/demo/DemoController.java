375daa6ceb422d16690da808ace3f25fe2f63e29
Paulius Kutka <13101751+pkey@users.noreply.github.com>
2019-02-19 16:14:03 +0200

    Mongo implementation

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Autowired
+    DemoRepository repo;
+
     @GetMapping(value = "/")
     public String helloWorld() {
-        return "Hello, World!";
+        List<Message> messages = repo.findAll();
+        Optional<Message> message = messages.stream().findFirst();
+        return message.get().message;
     }
 }
